5ed49cb5dff88b921529b1aa344bd747cadf1f0f
Ignacio <ignacio.urrutia@uc.cl>
2022-12-14 04:38:04 -0300

    parameters changes

--- Assets/Scripts/GlobalParameters.cs
+++ Assets/Scripts/GlobalParameters.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public static class GlobalParameters
 {
-    public static int trashPenalty = 3;         // For each trash
-    public static int stainPenalty = 10;        // For each stain
-    public static int furniturePenalty = 5;     // For each furniture out of place
+    public static int trashPenalty = 1;         // For each trash
+    public static int stainPenalty = 5;        // For each stain
+    public static int furniturePenalty = 2;     // For each furniture out of place
     public static int furnitureReward = 5;      // For each furniture in place
     public static int peopleReward = 5;        // For each person in the room
     public static int glassesReward = 10;       // For each glass in a table
